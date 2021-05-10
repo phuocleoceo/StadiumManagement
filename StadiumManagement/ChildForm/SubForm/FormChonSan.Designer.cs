6ebfad2f875dd775fa5f09b6b5ac8fcfffa1fe32
phuocleoceo <ht10082001@gmail.com>
2021-05-10 16:05:58 +0700

    Backup 2

--- StadiumManagement/ChildForm/SubForm/FormChonSan.Designer.cs
+++ StadiumManagement/ChildForm/SubForm/FormChonSan.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GUILayer.ChildForm.SubForm
 {
-    partial class FormChonDichVu
+    partial class FormChonSan
     {
         /// <summary>
         /// Required designer variable.
@@ -30,9 +30,20 @@ namespace GUILayer.ChildForm.SubForm
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lvSan = new System.Windows.Forms.ListView();
             this.imgList = new System.Windows.Forms.ImageList(this.components);
-            this.lvDV = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // lvSan
+            // 
+            this.lvSan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvSan.HideSelection = false;
+            this.lvSan.LargeImageList = this.imgList;
+            this.lvSan.Location = new System.Drawing.Point(0, 0);
+            this.lvSan.Name = "lvSan";
+            this.lvSan.Size = new System.Drawing.Size(664, 393);
+            this.lvSan.TabIndex = 1;
+            this.lvSan.UseCompatibleStateImageBehavior = false;
             // 
             // imgList
             // 
@@ -40,33 +51,22 @@ namespace GUILayer.ChildForm.SubForm
             this.imgList.ImageSize = new System.Drawing.Size(120, 120);
             this.imgList.TransparentColor = System.Drawing.Color.Transparent;
             // 
-            // lvDV
-            // 
-            this.lvDV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvDV.HideSelection = false;
-            this.lvDV.LargeImageList = this.imgList;
-            this.lvDV.Location = new System.Drawing.Point(0, 0);
-            this.lvDV.Name = "lvDV";
-            this.lvDV.Size = new System.Drawing.Size(664, 393);
-            this.lvDV.TabIndex = 0;
-            this.lvDV.UseCompatibleStateImageBehavior = false;
-            this.lvDV.SelectedIndexChanged += new System.EventHandler(this.lvDV_SelectedIndexChanged);
-            // 
-            // FormChonDichVu
+            // FormChonSan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 393);
-            this.Controls.Add(this.lvDV);
-            this.Name = "FormChonDichVu";
+            this.Controls.Add(this.lvSan);
+            this.Name = "FormChonSan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Chọn dịch vụ";
+            this.Text = "FormChonSan";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvSan;
         private System.Windows.Forms.ImageList imgList;
-        private System.Windows.Forms.ListView lvDV;
     }
 }
--- StadiumManagement/ChildForm/SubForm/FormChonSan.cs
+++ StadiumManagement/ChildForm/SubForm/FormChonSan.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLayer.Repository;
+﻿using BusinessLayer;
+using BusinessLayer.Repository;
 using BusinessLayer.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -9,25 +10,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BusinessLayer;
 
 namespace GUILayer.ChildForm.SubForm
 {
-    public partial class FormChonDichVu : Form
+    public partial class FormChonSan : Form
     {
-        private readonly ServiceRepository _db;
-        public FormChonDichVu()
+        private readonly StadiumRepository _db;
+        public FormChonSan()
         {
             InitializeComponent();
-            _db = new ServiceRepository();
+            _db = new StadiumRepository();
             LoadListView();
         }
 
         private void LoadListView()
         {
             int i = 0;
-            List<ServiceVM> listSVM = _db.GetList();
-            foreach(ServiceVM svm in listSVM)
+            List<StadiumVM> listSVM = _db.GetList();
+            foreach (StadiumVM svm in listSVM)
             {
                 Image img = svm.Image.ByteArrayToImage();
                 if (img != null)
@@ -36,19 +36,14 @@ namespace GUILayer.ChildForm.SubForm
                 }
                 else
                 {
-                    imgList.Images.Add(new Bitmap(120, 120));
+                    imgList.Images.Add(new Bitmap(120,120));
                 }
                 ListViewItem item = new ListViewItem();
                 item.Text = $"{svm.Name}\r\n{svm.Price}";
                 item.Tag = svm.Id;
-                item.ImageIndex = i++;  
-                lvDV.Items.Add(item);
+                item.ImageIndex = i++;
+                lvSan.Items.Add(item);
             }
-        }
-
-        private void lvDV_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
         }
     }
 }
